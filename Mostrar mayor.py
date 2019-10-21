60a9d60200db1fd9975a278d177a3ed95f5efe50
Kuranio <56848511+Kuranio@users.noreply.github.com>
2019-10-21 23:39:33 +0200

    Create Mostrar mayor.py

---
+++ Mostrar mayor.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Introduce un valor entero: "))
+numero2 = int(input("Introduce otro valor entero: "))
+if numero1 > numero2:
+    print("El mayor es:", numero1)
+else: 
+    print("El mayor es:", numero2)
