9fbe0179e131005831aa0925d53a9b89abf530f7
Kuranio <56848511+Kuranio@users.noreply.github.com>
2019-10-21 23:40:31 +0200

    Create Primero divisible por el segundo.py

---
+++ Primero divisible por el segundo.py
@@ -0,0 +1,6 @@
+entero1 = int(input("Introduce un valor entero: "))
+entero2 = int(input("Introduce otro valor: "))
+if entero1%entero2 == 0:
+    print(entero1,"es divisible por",entero2)
+elif entero1%entero2 != 0:
+    print(entero1,"no es divisible por",entero2)
