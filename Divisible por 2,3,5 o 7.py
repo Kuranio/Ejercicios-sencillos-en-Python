1d022ae8d733c5662e7c64537de048722033143a
Kuranio <56848511+Kuranio@users.noreply.github.com>
2019-10-21 23:41:34 +0200

    Create Divisible por 2,3,5 o 7.py

---
+++ Divisible por 2,3,5 o 7.py
@@ -0,0 +1,11 @@
+entero = int(input("Dame un valor entero: "))
+if entero%2==0:
+    print("2")
+elif entero%3==0:
+    print("3")
+elif entero%5==0:
+    print("5")
+elif entero%7==0:
+    print("7")
+else:
+    print("0")
